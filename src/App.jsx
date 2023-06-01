fcd5f11892b19748c6289e7f932acedaa48be865
cesar bonilla <cbonilla300@hotmail.com>
2023-06-01 02:21:21 -0400

    puppy data with usestate

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,18 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { puppyList } from './data'
 
 function App() {
-  
-
+  const [puppies , setpuppies] = useState(puppyList)
+  console.log(puppyList)
   return (
     <>
-      <div>
+      <div> {
+        puppies.map((puppy) => {
+          return <p key={puppy.id}>{puppy.name}</p>
+        })
+      }
         
       </div>
       
