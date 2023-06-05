2c6718db9cf93782bce99958420c7524628bdb5f
cesar bonilla <cbonilla300@hotmail.com>
2023-06-04 20:55:19 -0400

    final commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,21 +3,42 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import { puppyList } from './data'
+import './App.css';
+import './index.css';
 
 function App() {
   const [puppies , setpuppies] = useState(puppyList)
+  const [featPupId, setFeatPupId] = useState(null)
   console.log(puppyList)
+
+   const featuredPup = puppies.find((pup) => pup.id === featPupId)
+   console.log(featuredPup)
+   {featPupId &&(
+    <div>
+      <h2>{featuredPup.name}</h2>
+      <ul>
+        <li>Age: {featuredPup.age}</li>
+        <li>Email: {featuredPup.email}</li>
+      </ul>
+    </div>
+   )}
   return (
-    <>
-      <div> {
+    
+      
+      <div classname = "App"> {
         puppies.map((puppy) => {
-          return <p key={puppy.id}>{puppy.name}</p>
-        })
-      }
+          return( <p onClick={()=>{ setFeatPupId("puppy id:", puppy.id)}} key={puppy.id}>{puppy.name}</p>
+          
+   
+          ) 
+          
+      })
+        }
+      { featPupId && <p>{ featPupId }</p>}
         
       </div>
       
-    </>
+    
   )
 }
 
